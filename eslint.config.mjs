dd9cc713fb53f42f266fdf031e828d40be7e9e02
Eric Prasetya Sentosa <eric.sentosa@binus.ac.id>
2024-07-01 00:52:59 +0700

    Setup Project

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.jsx"],
+    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+  },
+  pluginReactConfig,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "no-unsafe-optional-chaining": "off",
+      "no-empty": "off",
+      "no-unused-vars": "off",
+    },
+  },
+];
